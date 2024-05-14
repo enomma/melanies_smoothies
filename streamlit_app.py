35121a977e07a9aa922c27af70ba3546013d6cc1
enomma <132085570+enomma@users.noreply.github.com>
2024-05-14 11:27:33 -0600

    Update streamlit_app.py
    added requests library to connect fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,11 @@ if ingredients_list:
         
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅" )
 
+#New session to display fruityvice nutricion information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
+
+
 
